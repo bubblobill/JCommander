a33d96169084878e9593665bad44e9193b6536d7
Zotyamester <szatmary.zoltan1222@gmail.com>
2023-10-03 21:57:50 +0200

    Extract icon loading to ResourceFactory

--- src/jcommander/ResourceFactory.java
+++ src/jcommander/ResourceFactory.java
@@ -1,2 +1,14 @@
-package jcommander;public class ResourceFactory {
+package jcommander;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ResourceFactory {
+
+    private static final int ICON_SIZE = 48;
+
+    public static ImageIcon getSquareIcon(String filename) {
+        Image image = new ImageIcon("images/" + filename).getImage();
+        return new ImageIcon(image.getScaledInstance(ICON_SIZE, ICON_SIZE, Image.SCALE_DEFAULT));
+    }
 }
