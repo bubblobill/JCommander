65ecec136c6fe5c8855b07e9c2cf3387e42d0b9b
Zotyamester <szatmary.zoltan1222@gmail.com>
2023-10-07 21:33:12 +0200

    Create distinguished view mode for mount points

--- src/jcommander/pane/directorylist/DirectoryListModel.java
+++ src/jcommander/pane/directorylist/DirectoryListModel.java
@@ -4,42 +4,20 @@ import javax.swing.*;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 import java.io.File;
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DirectoryListModel implements ListModel<File> {
 
-    // null-check avoidance + optimization technique:
-    //  - in order to avoid null-checks in @see getSize() and @see getElementAt(),
-    //    we maintain a steady, never-null list for the current directory's content;
-    //    thus even if for a given directory we can't list out its contents, we
-    //    propagate an empty collection of items instead of admitting the nullity
-    //  - a zero-length array can be shared across multiple instances, as there's
-    //    nothing to be shared there, except for the final *length* field;
-    //    therefore we can hold onto only one concrete zero-length array all-along
-    //    which should result in less allocation
-    private static final File[] defaultZeroList = new File[0];
-
-    File[] listOfFiles = DirectoryListModel.defaultZeroList;
+    private File[] files;
     private final List<ListDataListener> listeners = new ArrayList<>();
 
-
-    public void listDirectory(File directory) {
-        if (!directory.isDirectory()) {
-            throw new InvalidParameterException("Parameter must be a directory");
-        }
-
-        listOfFiles = directory.listFiles();
-        if (listOfFiles == null) {
-            listOfFiles = defaultZeroList;
-        }
-
-        notifyAllDirectoryChanged();
+    public void listDirectory(File[] files) {
+        this.files = files;
+        notifyAllContentsChanged(new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0, files.length));
     }
 
-    private void notifyAllDirectoryChanged() {
-        ListDataEvent e = new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0, listOfFiles.length);
+    private void notifyAllContentsChanged(ListDataEvent e) {
         for (ListDataListener listener : listeners) {
             listener.contentsChanged(e);
         }
@@ -47,12 +25,12 @@ public class DirectoryListModel implements ListModel<File> {
 
     @Override
     public int getSize() {
-        return listOfFiles.length;
+        return files.length;
     }
 
     @Override
     public File getElementAt(int index) {
-        return listOfFiles[index];
+        return files[index];
     }
 
     @Override
--- src/jcommander/pane/directorylist/FileCellRenderer.java
+++ src/jcommander/pane/directorylist/FileCellRenderer.java
@@ -2,11 +2,9 @@ package jcommander.pane.directorylist;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
 
-import static jcommander.ResourceFactory.getSquareIcon;
+import static jcommander.ResourceFactory.getIcon;
 
 public class FileCellRenderer implements ListCellRenderer<File> {
 
@@ -15,14 +13,20 @@ public class FileCellRenderer implements ListCellRenderer<File> {
     @Override
     public Component getListCellRendererComponent(JList<? extends File> list, File file, int index, boolean isSelected, boolean cellHasFocus) {
         JLabel cell = (JLabel) defaultRenderer.getListCellRendererComponent(list, file, index, isSelected, cellHasFocus);
-        cell.setText(file.getName());
 
-        if (file.isFile()) {
-            cell.setIcon(getSquareIcon("file.png"));
-            cell.setForeground(Color.BLACK);
+        if (file.getParentFile() == null) { // if it represents a mount point
+            cell.setText(file.getAbsolutePath());
+            cell.setIcon(getIcon("disk.png"));
         } else {
-            cell.setIcon(getSquareIcon("directory.png"));
-            cell.setForeground(Color.BLUE);
+            cell.setText(file.getName());
+
+            if (file.isFile()) {
+                cell.setIcon(getIcon("file.png"));
+                cell.setForeground(Color.BLACK);
+            } else {
+                cell.setIcon(getIcon("directory.png"));
+                cell.setForeground(Color.BLUE);
+            }
         }
 
         return cell;
